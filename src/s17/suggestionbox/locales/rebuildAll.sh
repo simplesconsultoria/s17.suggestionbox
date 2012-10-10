9209ebfbfff3397b70967aefc96d1327834c0e45
Gustavo Lepri <lepri@simplesconsultoria.com.br>
2012-10-10 16:55:19 -0300

    fix package i18n

--- src/s17/suggestionbox/locales/rebuildAll.sh
+++ src/s17/suggestionbox/locales/rebuildAll.sh
@@ -3,8 +3,8 @@
 set -e
 
 # first, create some pot containing anything
-i18ndude rebuild-pot --pot s17.content.suggestionbox.pot --create s17.content.suggestionbox --merge manual.pot ..
+i18ndude rebuild-pot --pot s17.suggestionbox.pot --create s17.suggestionbox --merge manual.pot ..
 
 # finally, update the po files
-i18ndude sync --pot s17.content.suggestionbox.pot  `find . -iregex '.*\.po$'|grep -v plone`
+i18ndude sync --pot s17.suggestionbox.pot  `find . -iregex '.*\.po$'|grep -v plone`
 
