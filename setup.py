324b21f6bcb6767a27619306b33eb1f842638db8
hvelarde <hector.velarde@gmail.com>
2013-07-10 14:01:58 -0300

    promote package status to beta

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='s17.suggestionbox',
       description="",
       long_description=long_description,
       classifiers=[
-          "Development Status :: 3 - Alpha",
+          "Development Status :: 4 - Beta",
           "Environment :: Web Environment",
           "Framework :: Plone",
           "Framework :: Plone :: 4.2",
